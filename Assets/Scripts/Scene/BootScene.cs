9a5f954c800a7edc737168c949523fa666ebf949
kudokoho <kudo04.aloedo@gmail.com>
2020-02-22 17:36:38 +0900

    Bootシーンあげもれ

---
+++ Assets/Scripts/Scene/BootScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneController.LoadScene("Title");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
