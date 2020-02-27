b08f901bb39c08ae8d03f3f5092f48207c820d62
kudokoho <kudo04.aloedo@gmail.com>
2020-02-27 23:22:33 +0900

    ヘルプ追加

---
+++ Assets/Scripts/Scene/Title/Help.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Help : MonoBehaviour
+{
+    /// <summary>
+    /// パネルを格納する変数
+    /// </summary>
+    [SerializeField] GameObject help_panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 操作方法ボタンが押されたときHelpPanelをアクティブにする
+    /// </summary>
+    public void SelectHelpOpenBotton()
+    {
+        help_panel.SetActive(true);
+    }
+
+    public void Close()
+    {
+        help_panel.SetActive(false);
+    }
+}
