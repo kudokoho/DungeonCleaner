9c64515853e1f2dcbadc9c8294ca4d16500c993f
kudokoho <kudo04.aloedo@gmail.com>
2020-03-02 23:06:36 +0900

    TitleScene編集

--- Assets/Scripts/Scene/Title/TitleScene.cs
+++ Assets/Scripts/Scene/Title/TitleScene.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
