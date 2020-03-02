637c25df5d2864985410cf0ce5842490985adc92
kudokoho <kudo04.aloedo@gmail.com>
2020-03-02 23:15:07 +0900

    Dungeonシーン共通処理追加

---
+++ Assets/Scripts/Scene/Dungeon/DungeonScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonScene : MonoBehaviour
+{
+    [SerializeField] CharacterBase player_ = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player_ == null) return;
+
+        player_.Move(true);
+
+    }
+}
