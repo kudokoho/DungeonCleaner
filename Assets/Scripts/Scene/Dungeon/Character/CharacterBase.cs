0e9b4eaa024ed92d0b5ccc8937a3573caa762302
kudokoho <kudo04.aloedo@gmail.com>
2020-03-02 23:15:27 +0900

    キャラクター共通処理追加

---
+++ Assets/Scripts/Scene/Dungeon/Character/CharacterBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    protected float speed_ = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(bool back = false)
+    {
+        Vector3 pos = transform.position;
+        if (back)
+        {
+            pos.x -= speed_;
+        } else
+        {
+            pos.x += speed_;
+        }
+        
+        transform.position = pos;
+    }
+}
