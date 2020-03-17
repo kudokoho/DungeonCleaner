2c5a296be38d4af1e66902991c49ce8a37f1a3e6
kudokoho <kudo04.aloedo@gmail.com>
2020-03-17 23:40:28 +0900

    CharacterBase　移動・ジャンプ処理追加

--- Assets/Scripts/Scene/Dungeon/Character/CharacterBase.cs
+++ Assets/Scripts/Scene/Dungeon/Character/CharacterBase.cs
@@ -4,30 +4,99 @@ using UnityEngine;
 
 public class CharacterBase : MonoBehaviour
 {
-    protected float speed_ = 1.0f;
+    protected float speed_ = 0.05f;
+    protected Rigidbody rigidbody_ = null;
+    protected Animator animator_ = null;
+    protected Vector3 jump_vec_ = new Vector3(0, 5, 0);
+
+    List<System.Action> action_list_ = new List<System.Action>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody_ = GetComponent<Rigidbody>();
+        rigidbody_.constraints = RigidbodyConstraints.FreezeRotation;
+
+        animator_ = GetComponentInChildren<Animator>();
+        StartChild();
     }
+
+    virtual protected void StartChild()
+    {
+
+    }
+
+    int counter = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        UpdateChild();
 
-    public void Move(bool back = false)
-    {
-        Vector3 pos = transform.position;
-        if (back)
+        if (action_list_.Count > 0)
         {
-            pos.x -= speed_;
+            action_list_[0]();
+            action_list_.RemoveAt(0);
         } else
         {
-            pos.x += speed_;
+            animator_.SetBool("walk", false);
         }
-        
+
+    }
+
+    virtual protected void UpdateChild()
+    {
+
+    }
+
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    public void Jump()
+    {
+        action_list_.Add(JumpAction);
+    }
+
+    void JumpAction()
+    {
+        //animator_.SetBool("jump", true);
+        if (rigidbody_ == null) return;
+        rigidbody_.velocity = jump_vec_;
+    }
+
+    /// <summary>
+    /// 移動(前進)
+    /// </summary>
+    public void MoveFront()
+    {
+
+        action_list_.Add(MoveFrontAction);
+    }
+
+    void MoveFrontAction()
+    {
+        animator_.SetBool("walk", true);
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+        Vector3 pos = transform.position;
+        pos.z += speed_;
+        transform.position = pos;
+    }
+
+    /// <summary>
+    /// 移動(後退)
+    /// </summary>
+    public void MoveBack()
+    {
+        action_list_.Add(MoveBackAction);
+    }
+
+
+    void MoveBackAction()
+    {
+        animator_.SetBool("walk", true);
+        transform.localRotation = Quaternion.Euler(0, 180, 0);
+        Vector3 pos = transform.position;
+        pos.z -= speed_;
         transform.position = pos;
     }
 }
