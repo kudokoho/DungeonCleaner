c12f86d6fc6158ae08d794d8485e760f8dedafed
kudokoho <kudo04.aloedo@gmail.com>
2020-06-16 20:45:32 +0900

    GameOver追加

---
+++ Assets/Scripts/Scene/Dungeon/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] GameObject gameover_ui_;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameover_ui_.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOverExec()
+    {
+        gameover_ui_.SetActive(true);
+        DungeonInfo.DungeonState = DungeonInfo.State.GameOver;
+    }
+
+    public void RetryButtonClick()
+    {
+
+    }
+
+    public void TitleButtonClick()
+    {
+        SceneController.LoadScene("Title");
+    }
+}
