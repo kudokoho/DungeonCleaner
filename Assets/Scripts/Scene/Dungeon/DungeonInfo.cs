35534f1c2bdc2ecdc05edbec95ede984fa45f34e
kudokoho <kudo04.aloedo@gmail.com>
2020-06-16 20:40:47 +0900

    DungeonInfo　追加

---
+++ Assets/Scripts/Scene/Dungeon/DungeonInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonInfo 
+{
+    public enum State
+    {
+        None,
+        GamePlay,
+        GameOver,
+        GameClear
+    }
+
+    public static State DungeonState = State.None;
+}
