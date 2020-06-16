be4bb973d1546ffb86952fb22143ca3b853d642c
kudokoho <kudo04.aloedo@gmail.com>
2020-06-16 20:42:09 +0900

    CharacterStatus　追加

---
+++ Assets/Scripts/Scene/Dungeon/Character/CharacterStatus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatus
+{
+    float power_ = 10;
+    float hp_ = 100;
+    float speed_ = 0.8f;
+
+    public float Power
+    {
+        get { return power_; }
+        set { power_ = value; }
+    }
+
+    public float HP
+    {
+        get { return hp_; }
+        set { hp_ = value; }
+    }
+
+    public float Speed
+    {
+        get { return speed_; }
+        set { speed_ = value; }
+    }
+}
