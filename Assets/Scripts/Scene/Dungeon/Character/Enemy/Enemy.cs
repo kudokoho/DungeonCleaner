4361d395da478175bd313cc020b781e9928f2598
kudokoho <kudo04.aloedo@gmail.com>
2020-06-16 20:39:19 +0900

    Enemy　スクリプト追加

---
+++ Assets/Scripts/Scene/Dungeon/Character/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : CharacterBase
+{
+
+    protected override void StartChild()
+    {
+        walk_animator_paramater_ = "";
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+
+         // 着地
+        if (collision.gameObject.tag == "Wepon" || collision.gameObject.tag == "Player")
+        {
+            if(collision.gameObject.GetComponent<CharacterBase>().State == CharacterBase.StateType.Attack)
+            {
+                // ダメージ処理
+                Debug.Log("プレーヤーの攻撃を受けた");
+                animator_.SetBool("TakeDamage", true);
+                Damage(collision.gameObject.GetComponent<CharacterBase>().Power);
+                KnockBack();
+            }
+            
+        }
+
+        base.OnCollisionEnter(collision);
+    }
+
+}
+
